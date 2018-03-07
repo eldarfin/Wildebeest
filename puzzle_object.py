92b10902e7be307f7522aadb657ea710096daddf
Taha Aksu <tahaaksu01@gmail.com>
2018-03-07 08:36:20 +0300

    puzzle object.v1

--- puzzle_object.py
+++ puzzle_object.py
@@ -1,2 +1,18 @@
+import numpy as np
 class puzzle_object:
-	
+	questions = np.chararray((2, 5))
+	questions[:]=" "
+	chars = np.chararray((5, 5))
+	chars[:] = ' '
+	answers = np.chararray((5, 5))
+	answers[:] = ' '
+	def _init__(self,questions,chars,answers):
+		self.questions=questions
+		self.chars=chars
+		self.answers=answers
+	def check(self):
+		return np.array_equal(chars,answers)
+	def fill(self, chr, x, y):
+		chars[x][y]= chr
+	def reveal(self):
+		chars = answers
