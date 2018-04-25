2714fc3f25702ce162de7f3bf4b9eec9555051b5
eldarfin <egeberkay12@gmail.com>
2018-04-25 17:12:28 +0300

    web_search_module updated

--- web_search_module.py
+++ web_search_module.py
@@ -4,7 +4,12 @@ from bs4 import BeautifulSoup
 import nltk
 import operator
 
-#
+def RepresentsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 nltk.download('punkt')
 stopwords = ['a','about','above','across','after','afterwards','again','against','all','almost','alone','along','already','also','although','always','am','among','an','and','another','any','anyhow','anyone','anything','anyway','anywhere','are','around','as','at','back','be','became','because','become','becomes','becoming','been','before','behind','being','below','beside','besides','between','beyond','both','bottom','but','by','call','can','cannot','cant','co','con','could','couldnt','did','didnt','do','does','doesnt','done','dont','down','due','during','each','either','else','elsewhere','enough','even','ever','every','everyone','everything','everywhere','except','few','fill','for','former','formerly','from','front','full','further','get','give','go','had','has','hasnt','have','havent','he','hence','her','here','hereafter','hereby','herein','hereupon','hers','herself','him','himself','his','how','however','i','if','ill','im','in','indeed','into','is','isnt','it','its','itself','ive','just','last','least','less','may','me','meanwhile','might','mine','more','moreover','most','mostly','move','much','must','my','myself','name','namely','neither','never','nevertheless','next','no','nobody','none','nor','not','nothing','now','nowhere','of','off','often','on','once','one','only','onto','or','other','others','otherwise','our','ours','ourselves','out','over','own','part','per','perhaps','please','put','rather','same','see','seem','seemed','seeming','seems','serious','she','should','shouldnt','side','since','so','some','somehow','someone','something','sometime','sometimes','somewhere','still','such','than','that','the','their','them','themselves','then','thence','there','thereafter','thereby','therefore','therein','thereupon','these','they','theyre','this','those','though','three','through','throughout','thru','thus','to','together','too','top','toward','towards','twelve','under','until','up','upon','us','very','was','we','well','were','weve','what','whatever','when','whence','whenever','where','whereas','whereby','wherein','whereupon','wherever','whether','which','while','whither','who','whoever','whole','whom','whose','why','will','with','within','without','wont','would','wouldnt','yet','you','your','youre','yours','yourself','yourselves','youve']
@@ -38,6 +43,12 @@ for i in range(len(texts)):
 for text in texts:
     for i in range(len(text)):
         text[i] = text[i].lower()
+
+for i in range(len(texts)):
+    texts[i] = [t for t in texts[i] if t not in stopwords]
+
+for i in range(len(texts)):
+    texts[i] = [t for t in texts[i] if not RepresentsInt(t)]
 
 counts = {}
 for text in texts:
